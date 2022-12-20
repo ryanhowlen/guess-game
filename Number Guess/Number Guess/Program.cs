2b4a03656ed981bdff37ba0226c5dc98bde7435b
ryanhowlen <64910678+ryanhowlen@users.noreply.github.com>
2022-12-20 16:14:28 +0000

    Add files via upload

---
+++ Number Guess/Number Guess/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Number_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int num = r.Next(1, 100);
+            int guesses = 5;
+
+            bool win = false;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter Guess:");
+                    int guess = Convert.ToInt32(Console.ReadLine());
+                    if (guess == num)
+                    {
+                        Console.WriteLine("You Got It!!");
+                        win = true;
+                    }
+                    else if (guess < num)
+                    {
+                        Console.WriteLine("Higher than that");
+                        guesses--;
+                        Console.WriteLine($"You have {guesses} guesses left");
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lower than that");
+                        guesses--;
+                        Console.WriteLine($"You have {guesses} guesses left");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Guess must be a number");
+                }
+
+            } while (win == false && guesses > 0);
+        }
+    }
+}
